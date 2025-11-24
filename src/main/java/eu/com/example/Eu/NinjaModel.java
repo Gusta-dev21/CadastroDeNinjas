41cca145a3d7cb35db8be00cedaa25726a44ac4e
unknown <gustavoazeribeiro21@gmail.com>
2025-11-24 14:27:42 -0300

    criei models

---
+++ src/main/java/eu/com/example/Eu/NinjaModel.java
@@ -0,0 +1,56 @@
+package eu.com.example.Eu;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cadastro_de_ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String email, int idade, String nome) {
+        this.email = email;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+    
+
+}
