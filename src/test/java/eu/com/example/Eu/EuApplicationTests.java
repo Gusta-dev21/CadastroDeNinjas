d29d01c70cc2f2507206326c55b4166bc501d433
unknown <gustavoazeribeiro21@gmail.com>
2025-11-24 13:12:04 -0300

    primeiro commit

---
+++ src/test/java/eu/com/example/Eu/EuApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.com.example.Eu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
